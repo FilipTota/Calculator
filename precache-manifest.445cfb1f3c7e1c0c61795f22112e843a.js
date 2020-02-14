90c165f6d70403a9b2f521238e4ab080cc6ae232
FilipTota <58394415+FilipTota@users.noreply.github.com>
2020-02-14 13:33:15 +0100

    Updates

---
+++ precache-manifest.445cfb1f3c7e1c0c61795f22112e843a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0d0208a6d02228ffaccb7aa255672d8",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "146cdf5a45462d65ea28",
+    "url": "/calculator/static/css/main.fa1317e4.chunk.css"
+  },
+  {
+    "revision": "b81b08c85c2d956a5e84",
+    "url": "/calculator/static/js/2.0af70cb3.chunk.js"
+  },
+  {
+    "revision": "fab1da7d8a32be3fe90e332131eacf28",
+    "url": "/calculator/static/js/2.0af70cb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "146cdf5a45462d65ea28",
+    "url": "/calculator/static/js/main.9eef0d45.chunk.js"
+  },
+  {
+    "revision": "28828353795638a97109",
+    "url": "/calculator/static/js/runtime-main.63ccf6e2.js"
+  }
+]);
